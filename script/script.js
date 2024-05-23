a07005fb7e93973fdc19d3479837491c2b44885a
rachana1711 <rachanashdas@gmail.com>
2024-05-23 17:21:17 +0530

    initial commit

---
+++ script/script.js
@@ -0,0 +1,33 @@
+const header = document.querySelector(".navbar")
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
+function toggleDescription(button) {
+    const cardBody = button.parentElement;
+    const description = cardBody.querySelector('.description');
+    const card = cardBody.parentElement.parentElement;
+    description.classList.toggle('expanded');
+    card.classList.toggle('expanded');
+}
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
